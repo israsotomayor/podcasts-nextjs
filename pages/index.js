ad9199dd6cc1018e7e94db8451202063535ecb6e
Israel Sotomayor <israel.sotomayor@eyetive.com>
2019-06-20 01:13:12 -0500

    Gitignore y otras configuraciones adicionales

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+export default class extends React.Component {
+    render () {
+        return <div>
+        <h1>Hola Platzi!!!</h1>
+        <p>Bienvenido al curso de Next.Js</p>
+
+        <img src="/static/platzi-logo.png" alt="Platzi" />
+
+        <style jsx>{`
+            h1 {
+                color: red;
+            }
+            :global(div p) {
+                color: green;
+            }
+            img {
+                max-width: 50%;
+                display: block;
+                margin: 0 auto;
+            }
+        `}</style>
+        <style jsx global>{`
+        body {
+            background: white;
+        }
+        `}</style>
+        </div>
+    }
+}
